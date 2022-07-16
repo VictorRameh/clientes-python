5a2b0ebb881de019f40373041912a8b3d59794f2
VictorRameh <vicrameh@gmail.com>
2022-07-16 12:45:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #View - o que vai para o usuário
-usuaio=input("Informe o seu usuário: ")
-senha=input("Informe sua senha: ")
+usuario_digitado=input("Informe o seu usuário: ")
+senha_digitado=input("Informe sua senha: ")
 
 # Model - O que vem do banco de dados (BD)
 usuário_BD= 'joão'
 senha_BD= '123'
 
 #Controller - a validação
-if usuário=='joão' and senha=='123':
+if usuario_digitado==usuário_BD and senha_digitado==senha_BD:
     print("pode entrar")
 else:
     print("Usuário ou senha incorreto")
